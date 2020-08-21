b40d8cc8e4137b27576e8007f5dd055ad48d6987
John Tsai <sheungon@gmail.com>
2020-08-21 12:11:07 +0800

    Updated sample.

---
+++ app/src/main/java/com/agehua/horizontalcoordinatordemo/HorizontalFloatingButtonBehaviour.kt
@@ -0,0 +1,49 @@
+package com.agehua.horizontalcoordinatordemo
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.LinearLayout
+import androidx.annotation.Keep
+import androidx.core.view.WindowInsetsCompat
+
+@Keep
+class HorizontalFloatingButtonBehaviour : ViewOffsetBehavior<LinearLayout> {
+
+    constructor()
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+
+    override fun layoutDependsOn(
+        parent: HorizontalCoordinatorLayout,
+        child: LinearLayout,
+        dependency: View
+    ): Boolean {
+        return dependency is HorizontalAppBarLayout
+    }
+
+    override fun onDependentViewChanged(
+        parent: HorizontalCoordinatorLayout,
+        child: LinearLayout,
+        dependency: View
+    ): Boolean {
+        val bottom = dependency.bottom
+        val center = bottom / 2f
+        val halfChild = child.height / 2f
+        val width = child.width
+        setTopAndBottomOffset((center - halfChild).toInt())
+        if (dependency is HorizontalAppBarLayout) {
+            val totalScrollRange = dependency.totalScrollRange.toFloat()
+            val movedPercentage = dependency.getRight() / totalScrollRange
+            child.x = -width * movedPercentage
+        }
+        return true
+    }
+
+    override fun onApplyWindowInsets(
+        coordinatorLayout: HorizontalCoordinatorLayout,
+        child: LinearLayout,
+        insets: WindowInsetsCompat
+    ): WindowInsetsCompat {
+        return super.onApplyWindowInsets(coordinatorLayout, child, insets)
+    }
+}
--- app/src/main/java/com/agehua/horizontalcoordinatordemo/test/TestActivity.kt
+++ app/src/main/java/com/agehua/horizontalcoordinatordemo/test/TestActivity.kt
@@ -3,8 +3,8 @@ package com.agehua.horizontalcoordinatordemo.test
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.agehua.horizontalcoordinatordemo.HorizontalAppBarLayout
 import com.agehua.horizontalcoordinatordemo.R
 
 class TestActivity : AppCompatActivity() {
@@ -12,6 +12,7 @@ class TestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
+
         val listView = findViewById<View>(R.id.list_view) as RecyclerView
         val data = arrayOf(
             TestData(R.drawable.ic_text_rotation_angleup_black_48dp, "0"),
@@ -49,5 +50,11 @@ class TestActivity : AppCompatActivity() {
         val adapter = MyRecyclerViewAdapter(listView)
         listView.adapter = adapter
         adapter.setData(listOf(*data))
+
+        val appbar = findViewById<HorizontalAppBarLayout>(R.id.appbar)
+        val floatingBtn = findViewById<View>(R.id.btn_app_bar_mini_floating)
+        floatingBtn.setOnClickListener {
+            appbar.setExpanded(true, true)
+        }
     }
 }
